57cba1d830f663ae33efc03770dbf70269b785e5
WilsonJWong <wilson.jw.wong@gmail.com>
2025-04-10 23:59:15 +1000

    .

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ import router from './router'
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
 
-import { db, auth } from './firebase/init.js'
-
 const app = createApp(App)
 app.use(PrimeVue, { theme: { preset: Aura } })
 app.use(router)
