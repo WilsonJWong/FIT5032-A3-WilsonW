e0fd0430f2c3fc5f7402009e7e38e212f6037077
WilsonJWong <wilson.jw.wong@gmail.com>
2025-04-11 00:27:44 +1000

    back

--- src/main.js
+++ src/main.js
@@ -1,39 +1,17 @@
-import './assets/main.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import router from './router'
 
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
-import router from './router'
 
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAZJQXYfVKdKDURAvO6RhsFEZrnwocSHR0",
-  authDomain: "ito5032-a3.firebaseapp.com",
-  projectId: "ito5032-a3",
-  storageBucket: "ito5032-a3.firebasestorage.app",
-  messagingSenderId: "994961541627",
-  appId: "1:994961541627:web:c747e473f4cf6a0bfde35e"
-};
-
-initializeApp(firebaseConfig);
+import { db, auth } from './firebase/init.js'
 
 const app = createApp(App)
-const db = getFirestore(app);
-const auth = getAuth(app);
-
-
-app.use(router)
-app.mount('#app')
-
 app.use(PrimeVue, { theme: { preset: Aura } })
+app.use(router)
 
-export { db, auth };
-
+app.mount('#app')
 
 
